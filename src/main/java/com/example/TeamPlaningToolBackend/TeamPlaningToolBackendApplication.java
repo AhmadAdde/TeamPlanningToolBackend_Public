b91c856f8b800a16568ee1af907a7b814835e065
Ahmad Al Khateeb <al.khateeb.ahmad96@gmail.com>
2023-04-03 14:25:50 +0200

    Initial commit

---
+++ src/main/java/com/example/TeamPlaningToolBackend/TeamPlaningToolBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.TeamPlaningToolBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamPlaningToolBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamPlaningToolBackendApplication.class, args);
+	}
+
+}
